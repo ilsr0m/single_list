73884cd722b1395833c85c90b0f3c694c269982e
ilsr0m <ilsrom@proton.me>
2026-03-12 22:00:35 +0300

    Added CreateTest. It checks list initialization

--- tests/great_test.cpp
+++ tests/great_test.cpp
@@ -4,6 +4,19 @@ extern "C"{
     #include "list_c.h"
 }
 
-TEST(ListTest, GreatTest){
-    EXPECT_EQ(1, 1);
+list_t *list;
+
+TEST(ListTest, CreateTest){
+    list = list_create(sizeof(int));
+    
+    EXPECT_NE(list, nullptr); // list is not null, coz it's already initialized
+    EXPECT_EQ(list->size, 0); // no any items
+    EXPECT_EQ(list->head, nullptr); // obviously null
+    EXPECT_EQ(list->tail, nullptr); // obviously null
+    EXPECT_EQ(list->item_size, sizeof(int)); // regards to the argument in list_create
+}
+
+int main(int argc, char **argv){
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
